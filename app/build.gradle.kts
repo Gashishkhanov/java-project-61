ffecc98b4c07c73970f1b2cf4ae9dc82fd2523a6
Gashishkhanov <zaur.ish005@gmail.com>
2024-01-28 19:23:05 +0300

    создал Makefile, исправил build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     application
+    kotlin("jvm")
 }
 
 application { mainClass.set("hexlet.code.App") }
@@ -15,6 +16,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
